2824c54545eaf038cff823fb4a83ec829fd8c7e0
Alena <58301797+AlenaOmsk@users.noreply.github.com>
2019-12-25 12:54:29 +0600

    Update app.py
    adding SQLAlchemy, Marshmallow

--- app.py
+++ app.py
@@ -1,2 +1,8 @@
 app = Flask(__name__)
 app.config.from_object(DevelopmentConfig)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+db.init_app(app)
+migrate = Migrate()
+migrate.init_app(app,db)
+ma = Marshmallow(app)
