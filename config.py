31a077111c442ec2c739725f6ca5a8a8705a50c1
Alena <58301797+AlenaOmsk@users.noreply.github.com>
2019-12-25 12:58:47 +0600

    Update config.py
    adding class Config (object)

--- config.py
+++ config.py
@@ -1,3 +1,12 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 import psycopg2
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
+    SECRET_KEY = 'this-really-needs-to-be-changed'
+    #db_url = "postgresql://Salem:1qazxsw23edc@localhost:5432/python_res" 
+    #SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://postgres:12345@localhost:5432/results"
